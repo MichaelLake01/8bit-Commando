2c1094876e67bc16b49f9e75b168b52d9f68b504
MichaelLake01 <michael.lake.av18@gmail.com>
2023-01-30 13:44:57 +0000

    Add basics vehicle classes and properties

---
+++ vehicles.py
@@ -0,0 +1,25 @@
+from weapon import *
+
+class Vehicles:
+    def __init__(self, weapon, state, exp):
+        self.weapon = weapon
+        self.state = state
+        self.exp = exp
+
+    def display(self):
+        print(f"Weapon: {self.weapon}")
+        print(f"Armor: {self.armor}")
+        print(f"Cover: {self.cover}")
+        print(f"State: {self.state}")
+        print(f"Veterancy: {self.exp}")
+
+class AttackHelicopter(Vehicles):
+    def __init_subclass__(self):
+        super().__init__((Minigun(), RocketLauncher()), 1, "Expert")
+
+
+class ChinookTransport(Vehicles):
+    def __init_subclass__(self):
+        super().__init__("None", 1, "Expert")
+
+
